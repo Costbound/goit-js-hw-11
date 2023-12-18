5cd40b4a7ad5e95877207935511ef10e8866a23b
Costbound <popovskiy96@gmail.com>
2023-12-18 16:31:19 +0100

    final

--- src/js/main.js
+++ src/js/main.js
@@ -1,9 +1,28 @@
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 
 const form = document.querySelector("form");
-const [searchInput, searchBtn] = form.elements;
+const [searchInput] = form.elements;
 const gallery = document.querySelector(".gallery");
+
+// Створення елементів бібліотек
+iziToast.settings({
+    message: "Sorry, there are no images matching your search query. Please try again!",
+    position: "topRight",
+    messageSize: "16px",
+    displayMode: 2,
+});
+const simpleGallery = new SimpleLightbox(".gallery a", {
+    captionsData: "alt",
+    captionDelay: 250,
+});
+const loader = document.createElement("span");
+loader.classList.add("loader");
+// створення елементів бібліотек
+
+
 
 const searchParams = new URLSearchParams({
     key: "40945002-e125ab8d3394997b1a8dc0871",
@@ -14,27 +33,85 @@ const searchParams = new URLSearchParams({
 
 form.onsubmit = async (e) => {
     e.preventDefault();
-    fetch(`https://pixabay.com/api/?${searchParams}&q=${searchInput.value}`)
+    // Запрос массиву картинок з сервера
+    const searchResponse = await fetch(`https://pixabay.com/api/?${searchParams}&q=${searchInput.value}`)
         .then(response => {
-            if (!response.ok) {
-                throw new Error(response.status);
-            }
+            if (!response.ok) throw new Error(response.status);
             return response.json();
         })
         .then(data => {
             if (data.hits.length === 0) {
-                iziToast.error();
+                return iziToast.error();
+            } else {
+                gallery.innerHTML = "";
+                gallery.append(loader);
+                return data.hits;
             }
         })
-        .catch(err => {
+        .catch(err => console.log(err));
+    
+    // Запрос та створення blob для кожної картинки за галереї
+    let results = await Promise.all(searchResponse.map(async (elem) => {
+        const  img  = await fetch(elem.webformatURL)
+            .then(response => {
+            if (!response.ok) throw new Error(response.status);
+            return response.blob();
+            })
+            .then(blob => blob)
+            .catch(err => console.log(err));
+        return img;
+    }))
+    
+    // Додавання елементів галереї до галереї
+    searchResponse.forEach(img => {
+        const { largeImageURL, tags, likes, views, comments, downloads } = img;
+        gallery.insertAdjacentHTML("beforeend",
+            `<li class="gallery-item">
+            <div class="img-wrapper">
+              <a href="${largeImageURL}">
+                  <img
+                    class="gallery-img" 
+                    alt="${tags}"
+                    width="360"
+                    height="200">
+              </a> 
+            </div>
+            <div class="text-wrapper">
+              <ul class="img-info-list">
+                <li class="info-item">
+                  <h3 class="info-title">likes</h3>
+                  <p class="info-text">${likes}</p>
+                </li>
+                <li class="info-item">
+                  <h3 class="info-title">views</h3>
+                  <p class="info-text">${views}</p>
+                </li>
+                <li class="info-item">
+                  <h3 class="info-title">comments</h3>
+                  <p class="info-text">${comments}</p>
+                </li>
+                <li class="info-item">
+                  <h3 class="info-title">downloads</h3>
+                  <p class="info-text">${downloads}</p>
+                </li>
+              </ul>
+            </div>
+        </li>`);
+    })
+
+    // Додавання src до зображень з blob
+    const pictures = document.querySelectorAll(".gallery-img");
+    pictures.forEach((picture, i) => {
+        try {
+            picture.src = URL.createObjectURL(results[i]);
+        } catch (err) {
             console.log(err);
-        });
+        }
+    });
+    simpleGallery.refresh();
+    loader.remove();
 }
 
-iziToast.settings({
-    message: "Sorry, there are no images matching your search query. Please try again!",
-    position: "topRight",
-    messageSize: "16px",
-    displayMode: 2,
-})
+
+
 
