b2c313895f95359edf4723d50f8c869ddf3e6f41
Costbound <145810315+Costbound@users.noreply.github.com>
2023-12-18 15:31:36 +0000

    Deploying to gh-pages from @ Costbound/goit-js-hw-11@5cd40b4a7ad5e95877207935511ef10e8866a23b 🚀

--- commonHelpers.js
+++ commonHelpers.js
@@ -1,2 +1,32 @@
-import{i}from"./assets/vendor-32231325.js";(function(){const t=document.createElement("link").relList;if(t&&t.supports&&t.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))s(e);new MutationObserver(e=>{for(const r of e)if(r.type==="childList")for(const o of r.addedNodes)o.tagName==="LINK"&&o.rel==="modulepreload"&&s(o)}).observe(document,{childList:!0,subtree:!0});function c(e){const r={};return e.integrity&&(r.integrity=e.integrity),e.referrerpolicy&&(r.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?r.credentials="include":e.crossorigin==="anonymous"?r.credentials="omit":r.credentials="same-origin",r}function s(e){if(e.ep)return;e.ep=!0;const r=c(e);fetch(e.href,r)}})();const a=document.querySelector("form"),[l,d]=a.elements;document.querySelector(".gallery");const u=new URLSearchParams({key:"40945002-e125ab8d3394997b1a8dc0871",image_type:"photo",orientation:"horizontal",safesearch:!0});a.onsubmit=async n=>{n.preventDefault(),fetch(`https://pixabay.com/api/?${u}&q=${l.value}`).then(t=>{if(!t.ok)throw new Error(t.status);return t.json()}).then(t=>{t.hits.length===0&&i.error()}).catch(t=>{console.log(t)})};i.settings({message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight",messageSize:"16px",displayMode:2});
+import{i as c,S as p}from"./assets/vendor-46aac873.js";(function(){const r=document.createElement("link").relList;if(r&&r.supports&&r.supports("modulepreload"))return;for(const e of document.querySelectorAll('link[rel="modulepreload"]'))i(e);new MutationObserver(e=>{for(const t of e)if(t.type==="childList")for(const s of t.addedNodes)s.tagName==="LINK"&&s.rel==="modulepreload"&&i(s)}).observe(document,{childList:!0,subtree:!0});function o(e){const t={};return e.integrity&&(t.integrity=e.integrity),e.referrerpolicy&&(t.referrerPolicy=e.referrerpolicy),e.crossorigin==="use-credentials"?t.credentials="include":e.crossorigin==="anonymous"?t.credentials="omit":t.credentials="same-origin",t}function i(e){if(e.ep)return;e.ep=!0;const t=o(e);fetch(e.href,t)}})();const f=document.querySelector("form"),[g]=f.elements,l=document.querySelector(".gallery");c.settings({message:"Sorry, there are no images matching your search query. Please try again!",position:"topRight",messageSize:"16px",displayMode:2});const y=new p(".gallery a",{captionsData:"alt",captionDelay:250}),a=document.createElement("span");a.classList.add("loader");const w=new URLSearchParams({key:"40945002-e125ab8d3394997b1a8dc0871",image_type:"photo",orientation:"horizontal",safesearch:!0});f.onsubmit=async n=>{n.preventDefault();const r=await fetch(`https://pixabay.com/api/?${w}&q=${g.value}`).then(e=>{if(!e.ok)throw new Error(e.status);return e.json()}).then(e=>e.hits.length===0?c.error():(l.innerHTML="",l.append(a),e.hits)).catch(e=>console.log(e));let o=await Promise.all(r.map(async e=>await fetch(e.webformatURL).then(s=>{if(!s.ok)throw new Error(s.status);return s.blob()}).then(s=>s).catch(s=>console.log(s))));r.forEach(e=>{const{largeImageURL:t,tags:s,likes:m,views:u,comments:h,downloads:d}=e;l.insertAdjacentHTML("beforeend",`<li class="gallery-item">
+            <div class="img-wrapper">
+              <a href="${t}">
+                  <img
+                    class="gallery-img" 
+                    alt="${s}"
+                    width="360"
+                    height="200">
+              </a> 
+            </div>
+            <div class="text-wrapper">
+              <ul class="img-info-list">
+                <li class="info-item">
+                  <h3 class="info-title">likes</h3>
+                  <p class="info-text">${m}</p>
+                </li>
+                <li class="info-item">
+                  <h3 class="info-title">views</h3>
+                  <p class="info-text">${u}</p>
+                </li>
+                <li class="info-item">
+                  <h3 class="info-title">comments</h3>
+                  <p class="info-text">${h}</p>
+                </li>
+                <li class="info-item">
+                  <h3 class="info-title">downloads</h3>
+                  <p class="info-text">${d}</p>
+                </li>
+              </ul>
+            </div>
+        </li>`)}),document.querySelectorAll(".gallery-img").forEach((e,t)=>{try{e.src=URL.createObjectURL(o[t])}catch(s){console.log(s)}}),y.refresh(),a.remove()};
 //# sourceMappingURL=commonHelpers.js.map
